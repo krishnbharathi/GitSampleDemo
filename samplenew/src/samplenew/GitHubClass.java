a78d4a1768700ecbab03f2c2f5b1ed13b1ff4fd8
krishnbharathi <krishnbharathi@gmail.com>
2016-07-25 16:37:05 +0530

    d

--- samplenew/src/samplenew/GitHubClass.java
+++ samplenew/src/samplenew/GitHubClass.java
@@ -5,8 +5,8 @@ public class GitHubClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Donec");
-		System.out.println("Donec");
-		System.out.println("Donec");
+		
+		System.out.println("master for changes ");
 	}
 
 }
